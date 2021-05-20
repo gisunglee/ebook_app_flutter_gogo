70f40b4d1e0af91fbba780cf0c1a6434f173fd70
= <479lgs@naver.com>
2021-05-20 20:04:12 +0900

    company

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -36,80 +36,191 @@ class DetailScreen extends StatelessWidget {
                     child: Column(
                       children: <Widget>[
                         SizedBox(height: size.height * .1),
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(
-                                    "Crushing &",
-                                    style: Theme.of(context).textTheme.display1,
-                                  ),
-                                  Text(
-                                    "Influence",
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .display1
-                                        .copyWith(
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                  ),
-                                  SizedBox(height: 5,),
-                                  Row(
-                                    children: <Widget>[
-                                      Expanded(
-                                        child: Column(
-                                          children: <Widget>[
-                                            Text(
-                                              "When the earth was flat and everyone wanted to win When the",
-                                              maxLines: 5,
-                                              style: TextStyle(
-                                                fontSize: 10,
-                                                color: kLightBlackColor,
-                                              ),
-                                            ),
-                                            SizedBox(
-                                              height: 5,
-                                            ),
-                                            RoundedButton(
-                                              text: "Read",
-                                              verticalPadding: 10,
-                                            )
-                                          ],
-                                        ),
-                                      ),
-                                      Column(
-                                        children: <Widget>[
-                                          IconButton(
-                                            icon: Icon(Icons.favorite_border),
-                                            onPressed: () {},
-                                          ),
-                                          BookRating(
-                                            score: 4.9,
-                                          ),
-                                        ],
-                                      )
-                                    ],
-                                  )
-                                ],
-                              ),
-                            ),
-                            Image.asset(
-                              "assets/images/book-1.png",
-                              height: 200,
-                            ),
-                          ],
-                        ),
+                        BookInfo(),
                       ],
                     ),
                   ),
                 ),
+                Padding(
+                  padding: EdgeInsets.only(top: size.height * .4 - 30),
+                  child: Column(
+                    children: <Widget>[
+                      ChapterCard(
+                        name: "Money",
+                        chapterNumber: 1,
+                        tag: "Life is about change",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Power",
+                        chapterNumber: 2,
+                        tag: "Everything loves power",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Influence",
+                        chapterNumber: 3,
+                        tag: "Influence easily like never before",
+                        press: () {},
+                      ),
+                      ChapterCard(
+                        name: "Win",
+                        chapterNumber: 4,
+                        tag: "Winning is what matters",
+                        press: () {},
+                      ),
+                      SizedBox(height: 10,),
+                    ],
+                  ),
+                ),
               ],
             ),
+
           ],
         ),
       ),
+    );
+  }
+}
+
+class ChapterCard extends StatelessWidget {
+  final String name;
+  final String tag;
+  final int chapterNumber;
+  final Function press;
+
+  const ChapterCard({
+    Key key,
+    this.name,
+    this.tag,
+    this.chapterNumber,
+    this.press,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 30),
+      margin: EdgeInsets.only(bottom: 16),
+      width: size.width - 48,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(38.5),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 10),
+            blurRadius: 33,
+            color: Color(0xFFD3D3D3).withOpacity(.84),
+          ),
+        ],
+      ),
+      child: Row(
+        children: <Widget>[
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "Chapter $chapterNumber : $name \n",
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: kBlackColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: tag,
+                  style: TextStyle(color: kLightBlackColor),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          IconButton(
+            icon: Icon(
+              Icons.arrow_forward_ios,
+              size: 18,
+            ),
+            onPressed: press,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BookInfo extends StatelessWidget {
+  const BookInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "Crushing &",
+                style: Theme.of(context).textTheme.display1,
+              ),
+              Text(
+                "Influence",
+                style: Theme.of(context).textTheme.display1.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          "When the earth was flat and everyone wanted to win When the",
+                          maxLines: 5,
+                          style: TextStyle(
+                            fontSize: 10,
+                            color: kLightBlackColor,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        RoundedButton(
+                          text: "Read",
+                          verticalPadding: 10,
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.favorite_border),
+                        onPressed: () {},
+                      ),
+                      BookRating(
+                        score: 4.9,
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+        Image.asset(
+          "assets/images/book-1.png",
+          height: 200,
+        ),
+      ],
     );
   }
 }
